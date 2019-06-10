581cc7a0313d05e6b7d9f28382606c91a7fd2fa0
Peter Bwire <bwire517@gmail.com>
2019-06-10 16:44:26 +0300

    Add db environment variables and clean cipher key

--- utils/database.go
+++ utils/database.go
@@ -9,15 +9,28 @@ import (
 	// Gorm relies on this dialect for initialization
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	otgorm "github.com/smacker/opentracing-gorm"
+	"fmt"
 )
 
 // ConfigureDatabase Database Access for environment is configured here
 func ConfigureDatabase(log *logrus.Entry) (*gorm.DB, error) {
 
-	dbDatasource := os.Getenv("DATABASE_URL")
 	dbDriver := os.Getenv("DATABASE_DRIVER")
 	if dbDriver == "" {
 		dbDriver = "postgres"
+	}
+
+	dbDatasource := os.Getenv("DATABASE_URL")
+	if(dbDatasource == ""){
+
+		dbHost := os.Getenv("DATABASE_HOST")
+		dbName := os.Getenv("DATABASE_NAME")
+		dbUserName := os.Getenv("DATABASE_USER_NAME")
+		dbSecret := os.Getenv("DATABASE_SECRET")
+		dbPort := os.Getenv("DATABASE_PORT")
+
+		dbDatasource = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s", dbHost, dbPort, dbUserName, dbSecret, dbName)
+
 	}
 
 	log.Debugf("Connecting using driver %v and source %v ", dbDriver, dbDatasource)
